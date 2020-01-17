9c3edf6faa97d0a14d48c9291bb9e5b180c4f751
Evan Bailey <embailey02@gmail.com>
2020-01-17 00:11:40 -0600

    Finished exercises.

--- src/sample_code/TicTacToe.java
+++ src/sample_code/TicTacToe.java
@@ -52,7 +52,8 @@ class TicTacToe {
 	public static final int HUMAN_WIN = 0;
 	public static final int DRAW = 1;
 	public static final int UNCLEAR = 2;
-	public static final int COMPUTER_WIN = 3;
+	public static final int COMPUTER_WIN_LAST = 3;
+	public static final int COMPUTER_WIN_IMMEDIATE = 12;
 
 	// Constructor
 	public TicTacToe() {
@@ -64,41 +65,56 @@ class TicTacToe {
 	}
 
 	public Best chooseMove(int side) {
-		return chooseMove(side, HUMAN_WIN, COMPUTER_WIN, 0);
+		return chooseMove(side, HUMAN_WIN, PRIORITIZE_IMMEDIACY ? COMPUTER_WIN_IMMEDIATE : COMPUTER_WIN_LAST, 0);
 	}
 
 	// Find optimal move
 	private Best chooseMove(int side, int alpha, int beta, int depth) {
 		int opp; // The other side
 		Best reply; // Opponent's best reply
-//		int dc; // Placeholder
+		// int dc; // Placeholder
 		int simpleEval; // Result of an immediate evaluation
 		Position thisPosition = new Position(board);
-		int tableDepth = 5; // Max depth placed in Trans. table
+		int tableDepth = 3; // Max depth placed in Trans. table
 		int bestRow = 0;
 		int bestColumn = 0;
 		int value;
 
-		if ((simpleEval = positionValue()) != UNCLEAR)
+		if ((simpleEval = positionValue(depth)) != UNCLEAR)
 			return new Best(simpleEval);
 
-		if (depth == 0)
-			transpositions.clear();
-		else if (depth >= 3 && depth <= tableDepth) {
-			Integer lookupVal = transpositions.get(thisPosition);
-			if (lookupVal != null)
-				return new Best(lookupVal);
+		if (USE_TRANSPOSITION_TABLE)
+			if (depth == 0)
+				transpositions.clear();
+			else if (depth >= 3 && depth <= tableDepth) {
+				Integer lookupVal = transpositions.get(thisPosition);
+				if (lookupVal != null)
+					return new Best(lookupVal);
+			}
+
+		if (ALPHA_BETA_PRUNING) {
+			if (side == COMPUTER) {
+				opp = HUMAN;
+				value = alpha;
+			}
+			else {
+				opp = COMPUTER;
+				value = beta;
+			}
+		}
+		else {
+			if (side == COMPUTER) {
+				opp = HUMAN;
+				value = HUMAN_WIN;
+			}
+			else {
+				opp = COMPUTER;
+				value = PRIORITIZE_IMMEDIACY ? COMPUTER_WIN_IMMEDIATE : COMPUTER_WIN_LAST;
+			}
 		}
 
-		if (side == COMPUTER) {
-			opp = HUMAN;
-			value = alpha;
-		} else {
-			opp = COMPUTER;
-			value = beta;
-		}
-
-		Outer: for (int row = 0; row < 3; row++)
+		// if (ALPHA_BETA_PRUNING)
+		Outer: for (int row = 0; row < 3; row++) {
 			for (int column = 0; column < 3; column++)
 				if (squareIsEmpty(row, column)) {
 					place(row, column, side);
@@ -113,15 +129,27 @@ class TicTacToe {
 
 						bestRow = row;
 						bestColumn = column;
-						if (alpha >= beta)
+						if (ALPHA_BETA_PRUNING && alpha >= beta)
 							break Outer; // Refutation
 					}
 				}
+			// }
+			// else
+			// for (int row = 0; row < 3; row++)
+			// for (int column = 0; column < 3; column++)
+			// if (squareIsEmpty(row, column)) {
+			// place(row, column, side);
+			// reply = chooseMove(opp, alpha, beta, depth + 1);
+			// place(row, column, EMPTY);
+			//
+			// if ()
+		}
 
-		if (depth <= tableDepth)
+		if (USE_TRANSPOSITION_TABLE && depth <= tableDepth)
 			transpositions.put(thisPosition, value);
 
 		return new Best(value, bestRow, bestColumn);
+
 	}
 
 	// Play move, including checking legality
@@ -179,7 +207,7 @@ class TicTacToe {
 	}
 
 	private Map<Position, Integer> transpositions = new HashMap<Position, Integer>();
-//	private Map<Position, Integer> transpositions = new TreeMap<Position, Integer>();
+	// private Map<Position, Integer> transpositions = new TreeMap<Position, Integer>();
 	private int[][] board = new int[3][3];
 
 	// Play a move, possibly clearing a square
@@ -191,8 +219,12 @@ class TicTacToe {
 		return board[row][column] == EMPTY;
 	}
 
+	private static final boolean PRIORITIZE_IMMEDIACY = true;
+	private static final boolean ALPHA_BETA_PRUNING = false;
+	private static final boolean USE_TRANSPOSITION_TABLE = false;
+
 	// Compute static value of current position (win, draw, etc.)
-	private int positionValue() {
-		return isAWin(COMPUTER) ? COMPUTER_WIN : isAWin(HUMAN) ? HUMAN_WIN : boardIsFull() ? DRAW : UNCLEAR;
+	private int positionValue(int depth) {
+		return isAWin(COMPUTER) ? PRIORITIZE_IMMEDIACY ? COMPUTER_WIN_IMMEDIATE - depth : COMPUTER_WIN_LAST : isAWin(HUMAN) ? HUMAN_WIN : boardIsFull() ? DRAW : UNCLEAR;
 	}
 }
